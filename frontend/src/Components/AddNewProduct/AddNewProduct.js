f055ab22709ab2b570ab4cc2232f4cf2403b20c7
Zahra Gholipour <z.gholipour.work@gmail.com>
2023-10-06 19:23:52 +0330

    add new product section updated

--- frontend/src/Components/AddNewProduct/AddNewProduct.js
+++ frontend/src/Components/AddNewProduct/AddNewProduct.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddNewProduct.css";
 
 export default function AddNewProduct() {
+  const [newProductTitle, setNewProductTitle] = useState("");
+  const [newProductPrice, setNewProductPrice] = useState("");
+  const [newProductCount, setNewProductCount] = useState("");
+  const [newProductImg, setNewProductImg] = useState("");
+  const [newProductPopularity, setNewProductPopularity] = useState("");
+  const [newProductSale, setNewProductSale] = useState("");
+  const [newProductColors, setNewProductColors] = useState("");
+
+  const newProductsInfos = {
+    title: newProductTitle,
+    price: newProductPrice,
+    count: newProductCount,
+    img: newProductImg,
+    popularity: newProductPopularity,
+    sale: newProductSale,
+    colors: newProductColors,
+  };
+
+  const addNewProduct = (event) => {
+    event.preventDefault();
+    fetch(`http://localhost:8000/api/products/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProductsInfos),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => console.log(data));
+  };
   return (
     <div className="product-main">
       <h1 className="product-title">افزودن محصول جدید</h1>
@@ -12,6 +44,10 @@ export default function AddNewProduct() {
               type="text"
               className="add-products-input"
               placeholder="نام محصول را بنویسید"
+              value={newProductTitle}
+              onChange={(event) => {
+                setNewProductTitle(event.target.value);
+              }}
             />
           </div>
           <div className="add-products-form-group">
@@ -19,6 +55,10 @@ export default function AddNewProduct() {
               type="text"
               className="add-products-input"
               placeholder="قیمت محصول را بنویسید"
+              value={newProductPrice}
+              onChange={(event) => {
+                setNewProductPrice(event.target.value);
+              }}
             />
           </div>
           <div className="add-products-form-group">
@@ -26,6 +66,10 @@ export default function AddNewProduct() {
               type="text"
               className="add-products-input"
               placeholder="موجودی محصول را بنویسید"
+              value={newProductCount}
+              onChange={(event) => {
+                setNewProductCount(event.target.value);
+              }}
             />
           </div>
           <div className="add-products-form-group">
@@ -33,13 +77,32 @@ export default function AddNewProduct() {
               type="text"
               className="add-products-input"
               placeholder="آدرس عکس را بنویسید"
+              value={newProductImg}
+              onChange={(event) => {
+                setNewProductImg(event.target.value);
+              }}
             />
           </div>
           <div className="add-products-form-group">
             <input
               type="text"
               className="add-products-input"
+              placeholder="میزان محبوبیت محصول را بنویسید"
+              value={newProductPopularity}
+              onChange={(event) => {
+                setNewProductPopularity(event.target.value);
+              }}
+            />
+          </div>{" "}
+          <div className="add-products-form-group">
+            <input
+              type="text"
+              className="add-products-input"
               placeholder="میزان فروش محصول را بنویسید"
+              value={newProductSale}
+              onChange={(event) => {
+                setNewProductSale(event.target.value);
+              }}
             />
           </div>
           <div className="add-products-form-group">
@@ -47,10 +110,16 @@ export default function AddNewProduct() {
               type="text"
               className="add-products-input"
               placeholder=" تعداد رنگ بندی محصول را بنویسید"
+              value={newProductColors}
+              onChange={(event) => {
+                setNewProductColors(event.target.value);
+              }}
             />
           </div>
         </div>
-        <button className="add-product-btn">ثبت محصول</button>
+        <button className="add-product-btn" onClick={addNewProduct}>
+          ثبت محصول
+        </button>
       </form>
     </div>
   );
