0b2ef244e220e8ddc56d3933245cb0e23fe33e19
Zahra Gholipour <z.gholipour.work@gmail.com>
2023-10-07 21:22:50 +0330

    comment section updated

--- frontend/src/Components/Comments/Comments.js
+++ frontend/src/Components/Comments/Comments.js
@@ -1,12 +1,49 @@
-import React from "react";
-import DeleteModal from "../DeleteModal/DeleteModal";
+import React, { useEffect, useState } from "react";
 import ErrorBox from "../ErrorBox/ErrorBox";
 
 export default function Comments() {
+  const [allComments, setAllComments] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:8000/api/comments/`)
+      .then((res) => res.json())
+      .then((comments) => setAllComments(comments));
+  }, []);
   return (
-    <>
-      <ErrorBox msg="هیچ کامنتی یافت نشد" />
-      <DeleteModal />
-    </>
+    <div className="cms-main">
+      {allComments.length ? (
+        <table className="cms-table">
+          <thead>
+            <tr>
+              <th>اسم کاربر</th>
+              <th>محصول</th>
+              <th>متن کامنت</th>
+              <th>تاریخ ثبت</th>
+              <th>ساعت ثبت</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allComments.map((comment) => (
+              <tr key={comment.id}>
+                <td>{comment.userID}</td>
+                <td>{comment.productID}</td>
+                <td>
+                  <button>دیدن متن</button>
+                </td>
+                <td>{comment.date}</td>
+                <td>{comment.hour}</td>
+                <td>
+                  <button>حذف</button>
+                  <button>ویرایش</button>
+                  <button>پاسخ</button>
+                  <button>تایید</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <ErrorBox msg="هیچ کامنتی یافت نشد" />
+      )}
+    </div>
   );
 }
